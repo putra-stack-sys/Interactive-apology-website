3cf0c8a1021da5d78947d388baa56535a751acab
putra-stack-sys <putradanishiskandarbinsuhaini@gmail.com>
2026-05-23 01:26:39 +0800

    mine

--- src/data/story.js
+++ src/data/story.js
@@ -6,23 +6,23 @@ import img5 from "../assets/images/img5.jpg";
 
 export const story = [
   {
-    image: img1,
+    image: "/src/assets/images/img1.jpg",
     text: "Hiiii Sayanggg, Just wanted to tell you somethingggg...",
   },
   {
-    image: img2,
+    image: "/src/assets/images/img2.jpg",
     text: "Do you remember this picture? Our first picture that you took ✨",
   },
   {
-    image: img3,
+    image: "/src/assets/images/img3.jpg",
     text: "Our first date... 🥺",
   },
   {
-    image: vid4,
+    image: "/src/assets/images/vid4.mp4",
     text: "A day full of enjoyymenttt 😌",
   },
   {
-    image: img5,
+    image: "/src/assets/images/img5.jpg",
     text: "Yeah, sometimes I do things that make you angry, and I am sorry about that, now i already change myself for you sayang i hope that we can be happy together again ❤️",
   },
 ];
